83a4a9236b27f0bfb450a6e5e59ec038cd88b0d2
Vishwas Kancharlapalli <vishwas.kancharlapalli@gmail.com>
2023-10-29 11:52:26 +0000

    cleaned and fixed error

--- app/routes/questions.py
+++ app/routes/questions.py
@@ -5,12 +5,17 @@ questions = Blueprint("questions", __name__)
 
 @questions.route("/get-question", methods=["GET"])
 def get_question():
+    
     try:
+        list = set([])
         db = app.mongo['greatunihack']
         collection = db['questions']
         collection_size = collection.count_documents({})
         rand_ques_id = random.randrange(1,collection_size)
+        if rand_ques_id in list:
+            rand_ques_id = random.randrange(1,collection_size)
         random_question = collection.find_one({"id": rand_ques_id})
+
         return jsonify({"id": rand_ques_id, "question": random_question["questionBody"], "choice1": random_question["1"], "choice2": random_question["2"], "choice3": random_question["3"], "choice4": random_question["4"]})
     except Exception as e:
         return jsonify(str(e))
@@ -24,57 +29,6 @@ def post_compare():
 
         db = app.mongo['greatunihack']
         collection = db['questions']
-        result = collection.find_one({"id": q_id})
-
-        return jsonify(str(user_guess) == result["answer"])
-    except Exception as e:
-        return jsonify(str(e))
-
-
-@questions.route("/connect", methods=['POST'])
-def post_compare():
-    try:
-        received_data = request.get_json()
-        username = received_data["username"]
-        password = received_data["password"]
-        query = {
-            "username": username,  # 替换为第一个字段的名称
-            "password": password   # 替换为第二个字段的名称
-        }
-        db = app.mongo['greatunihack']
-        collection = db['username']
-        user = collection.find(query)
-        print(collection)
-
-    except Exception as e:
-        return jsonify(str(e))
-    
-@questions.route("/disconnect", methods=['POST'])
-def post_compare():
-    try:
-        received_data = request.get_json()
-        username = received_data["username"]
-        password = received_data["password"]
-        query = {
-            "username": username,  # 替换为第一个字段的名称
-            "password": password   # 替换为第二个字段的名称
-        }
-        db = app.mongo['greatunihack']
-        collection = db['username']
-        user = collection.find(query)
-
-    except Exception as e:
-        return jsonify(str(e))
-    
-@questions.route("/match", methods=['GET'])
-def post_compare():
-    try:
-        received_data = request.get_json()
-        username = received_data["username"]
-        password = received_data["password"]
-        
-        db = app.mongo['greatunihack']
-        collection = db['username']
         result = collection.find_one({"id": q_id})
 
         return jsonify(str(user_guess) == result["answer"])
